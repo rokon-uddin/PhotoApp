1640d71d78f2749ac7cc530d89a55867cf5e1a17
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2024-10-06 14:08:23 +0600

    Initial Commit

---
+++ PhotoApp/PhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppApp.swift
+//  PhotoApp
+//
+//  Created by Mohammed Rokon Uddin on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
